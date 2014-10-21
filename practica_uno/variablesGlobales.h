eb9309996d754a55f89eac6d160e87a15220fd88
Aitor <aiturrioz005@ikasle.ehu.es>
2014-10-21 11:28:02 +0200

    Añadida una nueva variable global llamada MODO que determinará si usamos la proyección ortogonal o la de la cámara.

--- practica_uno/variablesGlobales.h
+++ practica_uno/variablesGlobales.h
@@ -12,6 +12,7 @@ extern float	SELECTED_RED;
 extern float	SELECTED_GREEN;
 extern float	SELECTED_BLUE;
 extern int		ANCHO, ALTO, color;
+extern int		MODO;
 extern float	min_x, min_y, min_z, max_x, max_y, max_z;
 extern struct 	NODO * nodo_actual;
 
